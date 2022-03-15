932b8b7bb539b90c1fe09038ce288c8711ef88a5
Paul Lu <paullu@ualberta.ca>
2022-03-15 09:56:02 -0600

    Paul

---
+++ Lecture.17/receiver-hp.cpp
@@ -0,0 +1,42 @@
+#include <iostream> 
+#include <unistd.h> 
+#include <fcntl.h> 
+#include <sys/types.h> 
+#include <sys/stat.h>
+#include <string.h>
+#define _MSG_MAX_LENGTH 100
+
+using namespace std;
+
+struct header {
+  unsigned int len;     // Ignoring byte ordering for now
+};
+
+int main() {
+
+  const char * pipename = "./sharedch";
+
+  mkfifo(pipename, 0666); // all users can read and write 
+  int fd = open(pipename, O_RDONLY); // open for read-only access
+
+  char msg[_MSG_MAX_LENGTH];
+  struct header h = { 0 };
+
+  while(true){
+    memset(msg, 0, _MSG_MAX_LENGTH);
+
+    // finish code here
+
+    if( strcmp(msg, "quit") == 0 ){
+      break;
+    }
+  }
+
+  // close pipe from the write end 
+  close(fd);
+
+  // reclaim the backing file 
+  unlink(pipename);
+
+  return 0;
+}
